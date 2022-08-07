4f12ed946bdb95c96ee557870e09fd0fa14b7510
Sultonbek Nazarov <sultonbeknazarov@Sultonbeks-MacBook-Pro.local>
2022-08-07 11:54:59 +0500

    clean up

--- src/styles/global-styles.ts
+++ src/styles/global-styles.ts
@@ -2,7 +2,7 @@ import { createGlobalStyle } from "styled-components/macro"
 import spaceGrotesk from "./fonts/SpaceGrotesk.ttf"
 
 export enum CssVariables {
-  /* Here starts cns colors */
+  /* Here starts skeleton colors */
 
   /** #1B1B1B */
   Main = "var(--main)",
@@ -34,6 +34,11 @@ export const GlobalStyle = createGlobalStyle`
   }
 
 
+  :root {
+    --main: #1B1B1B;
+  }
+
+
   html,
   body {
     height: 100%;
@@ -47,75 +52,7 @@ export const GlobalStyle = createGlobalStyle`
       text-decoration: none;
   }
 
-  :root {
-    --main: #1B1B1B;
-  }
-
-  a {
-      text-decoration: none;
-  }
-
   ul {
       list-style: none;
   }
-
-  ::-webkit-scrollbar {
-      width: 1px;
-      height: 1px;
-      background-color: var(--main);
-  }
-
-
-  /*vertical Track */
-
-  ::-webkit-scrollbar-track:vertical {
-      /* box-shadow: inset 0 0 3px grey; */
-      border-radius: 10px;
-  }
-
-
-  /*vertical Handle */
-
-  ::-webkit-scrollbar-thumb:vertical {
-      background: var(--black);
-      border-radius: 10px;
-  }
-
-
-  /*horizontal Track */
-
-  ::-webkit-scrollbar-track:horizontal {
-      box-shadow: inset 0 0 3px var(--main);
-      border-radius: 10px;
-  } 
-
-
-  /*horizontal Handle */
-
-  ::-webkit-scrollbar-thumb:horizontal {
-      background: var(--main);
-      border-radius: 10px;
-  }
-
-
-  /* Handle on hover */
-
-  ::-webkit-scrollbar-thumb:hover {
-      background: var(--main);
-  }
-
-  ::selection {
-      background: var(--main);
-      color: var(--main);
-  }
-
-  ::-moz-selection {
-      background: var(--main);
-      color: var(--main);
-  }
-
-  * {
-      scrollbar-width: thin;
-  }
-
 `
